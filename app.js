a94ff86a89b508fa5662ec6248b8c854a320c4dd
Rashi Dhar <rdhar1997@gmail.com>
2020-07-16 12:20:51 -0400

    feat: 🎸 changes to experiment end trial, USE_PHOTODIODE

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const trials = './trials';
 
+var config = { USE_PHOTODIODE: false,  AT_HOME: false, IS_ELECTRON: true}
+
+
+
 // fs.readdir(trials, (err, files) => {
 //   files.forEach(file => {
 //     console.log(require(trials+'/'+file));
 //   });
 // });
-// console.log(require('./trials/experimentStart.js'));
-exports.require('./trials/countdown.js');
+// console.log(require('./trials/experimentEnd.js')(100,'This is end',true));
+module.exports = { 
+    countdown: require('./trials/countdown.js'),
+    experimentEnd: require('./trials/experimentEnd.js')(100,'This is end',true)
+};
