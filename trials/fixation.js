0d85d713636dffcd2471fc203778fbb1aba028e0
Rashi Dhar <rdhar1997@gmail.com>
2020-07-29 12:39:55 -0400

    feat: 🎸 remove unwanted code and modify fixation

--- trials/fixation.js
+++ trials/fixation.js
@@ -1,21 +1,24 @@
-const { jitter50 } = require('../lib/utils')
 const { pdSpotEncode, photodiodeGhostBox } = require('../lib/markup/photodiode')
-const { fixationHTML } = require('../lib/markup/fixation')
 
-const fixation = (duration) => {
-  let stimulus = fixationHTML + photodiodeGhostBox()
+const defaultbuttons = [];
 
-  const code = eventCodes.fixation;
+module.exports = function(duration, config, responseEndsTrial = false, taskCode = null, numBlinks = 1, buttons = defaultbuttons) {
+  const jitter = (base, offset) => (
+    base + Math.floor(Math.random() * Math.floor(offset))
+  )
+  const jitter50 = (base) => jitter(base, 50)
+  
+  let stimulus = '<div class="beads_container"><div id="fixation-dot"> </div></div>' 
+  if(config.USE_PHOTODIODE) stimulus += photodiodeGhostBox();
 
   return {
     type: 'html_keyboard_response',
-    choices: jsPsych.NO_KEYS,
+    choices: buttons,
     stimulus: stimulus,
-    response_ends_trial: false,
+    response_ends_trial: responseEndsTrial,
     trial_duration: jitter50(duration),
-    on_load: () => pdSpotEncode(code),
-    on_finish: (data) => data.code = code
+    on_load: () => pdSpotEncode(taskCode, numBlinks),
+    on_finish: (data) => data.taskCode = taskCode
   }
 }
 
-export default fixation
