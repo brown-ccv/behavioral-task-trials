5e49b0be372272a37b7c8c7173ce81143083dba3
Rashi Dhar <rdhar1997@gmail.com>
2020-08-07 07:46:34 -0400

    feat: 🎸 change app.js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const trials = './trials';
 const _ = require('lodash')
 
-const functions = {}
 const defaultconfig = { USE_PHOTODIODE: false,  USE_EEG: false, IS_ELECTRON: true, USE_MTURK: false}
 
-functions['init'] = function(_config){
-  return _.extend(defaultconfig,_config)
-}
 
-
-fs.readdirSync(trials)
-.filter(file => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
-.map((file) => {functions[file.slice(0, -3)] = require(trials+'/'+file)})
-
-module.exports = functions;
+module.exports = { 
+    init: function(_config){
+        return _.extend(defaultconfig,_config)
+    },
+    countdown: require('./trials/countdown.js'),
+    showMessage: require('./trials/showMessage.js'),
+    showEarnings: require('./trials/showEarnings.js'),
+    fixation: require('./trials/fixation.js'),
+    userId: require('./trials/userId.js')
+};
