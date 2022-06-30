c6194d1d5a25cabb7995429bb57c63aaa91571d8
Benjamin Heasly <benjamin.heasly@gmail.com>
2022-06-30 15:31:01 -0400

    fix: 🐛 Compare jspsych plugin types by name
    Fixes comparison between jspsych plugin types, now compares by name
    rather than raw value.

--- trials/showMessage.js
+++ trials/showMessage.js
@@ -18,26 +18,26 @@ import { baseStimulus } from "../lib/markup/stimuli"
  * @param {boolean} config.USE_MTURK - USE_MTURK flag
  * @param {Object} options
  * @param {string} options.responseType - This tells jsPsych which plugin file to use to run the trial. (default: htmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response")
- * @param {number} options.duration - trial duration in milliseconds, only to be specified if the options.responseType is htmlKeyboardResponse. (default: 1000, if options.responseType ===  htmlKeyboardResponse, otherwise ignored)
+ * @param {number} options.duration - trial duration in milliseconds, only to be specified if the options.responseType is htmlKeyboardResponse. (default: 1000, if options.responseType is htmlKeyboardResponse, otherwise ignored)
  * @param {string} options.stimulus - Onscreen stimulus in HTML to be shown in the trial, if not set default text is empty. If the stimulus is not provided, message should be provided as a string. (default: "")
  * @param {string} options.message - Onscreen message to be shown in the trial. (default: "")
  * @param {boolean} options.onstart - True if the message is to be display on start of the trial. False if the message needs to be in the stimulus.(default: false)
  * @param {boolean} options.responseEndsTrial - True if the trial ends on response,false if the trial waits for the duration. (default: false)
  * @param {number} options.taskCode - Task code to be saved into data log (default: 1)
  * @param {number} options.numBlinks - Number of times the pulse needs to be repeated for photodiode box, when USE_PHOTODIODE is set true. (default: 1)
- * @param {Array} options.buttons - This array contains the keys that the subject is allowed to press in order to respond to the stimulus. Keys can be specified as their numeric key code or as characters (e.g., 'a', 'q'). The default value of "ALL_KEYS" means that all keys will be accepted as valid responses. Specifying "NO_KEYS" will mean that no responses are allowed. Only to be specified if the options.responseType is htmlButtonResponse from "@jspsych/plugin-html-button-response" (default: ["OK"], if options.responseType === htmlButtonResponse, otherwise ignored)
+ * @param {Array} options.buttons - This array contains the keys that the subject is allowed to press in order to respond to the stimulus. Keys can be specified as their numeric key code or as characters (e.g., 'a', 'q'). The default value of "ALL_KEYS" means that all keys will be accepted as valid responses. Specifying "NO_KEYS" will mean that no responses are allowed. Only to be specified if the options.responseType is htmlButtonResponse from "@jspsych/plugin-html-button-response" (default: ["OK"], if options.responseType is htmlButtonResponse, otherwise ignored)
  */
 export function showMessage(config, options) {
   const defaults = {
     responseType: htmlKeyboardResponse,
-    duration: options.responseType == htmlButtonResponse ? null : 1000,
+    duration: (options.responseType && options.responseType.name == htmlButtonResponse.name) ? null : 1000,
     stimulus: "",
     message: "",
     onstart: false,
     responseEndsTrial: false,
     taskCode: 1,
     numBlinks: 1,
-    buttons: options.responseType == htmlKeyboardResponse ? null : ["OK"],
+    buttons: (options.responseType && options.responseType.name == htmlKeyboardResponse.name) ? null : ["OK"],
   };
   const {
     responseType,
